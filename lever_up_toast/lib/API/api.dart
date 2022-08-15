5baadffd92f515d75332ec6841932f5adf759ff3
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-08-15 21:07:07 +0900

    save

---
+++ lever_up_toast/lib/API/api.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Api with ChangeNotifier{
+
+  void loginAPI(Map<String, dynamic> data, BuildContext context)async{
+    
+  }
+}
---
+++ lever_up_toast/lib/API/apiInfo.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: file_names
+
+class ApiInfo {
+  static const String loginUri =  "http://210.123.254.201:8080/";
+
+}
