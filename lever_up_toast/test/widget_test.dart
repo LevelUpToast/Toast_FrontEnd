98d302e0b57217335fcf2f1a3df38bbd5c126029
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-08-15 13:51:43 +0900

    save

--- lever_up_toast/test/widget_test.dart
+++ lever_up_toast/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lever_up_toast/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LUP());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
