144881b583878172214e9927bdfe8bd46ccff22c
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-10-25 11:35:35 +0900

    pay

--- lever_up_toast/lib/screens/Pay/toastPay.dart
+++ lever_up_toast/lib/screens/Pay/toastPay.dart
@@ -14,44 +14,132 @@ class ToastPayScreen extends StatefulWidget {
 class _ToastPayScreenState extends State<ToastPayScreen> {
   @override
   Widget build(BuildContext context) {
+    Map toastPay =    {
+      "payName": "ToastPay",
+      "payCheck": "100,000",
+      "color": Color(0xFF4C7241)
+    };
+    List payment = [
+      {"card": "VISA", "cardNum": "1234-1234-1234-1234", "color": Colors.blue},
+      {"card": "VISA", "cardNum": "1234-1234-1234-1234", "color": Colors.cyan},
+    ];
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        actions :  [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: InkWell(
-              child: const Icon(Icons.home,color: Colors.black,),
-              onTap: () => Navigator.pushReplacementNamed(context, '/home'),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: InkWell(
+                child: const Icon(
+                  Icons.home,
+                  color: Colors.black,
+                ),
+                onTap: () => Navigator.pushReplacementNamed(context, '/home'),
+              ),
+            ),
+          ],
+          elevation: 0,
+          title: const Text(
+            'LevelUpToast',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
             ),
           ),
-        ],
-        elevation: 0,
-        title: const Text('LevelUpToast',
-          style: TextStyle(
+          iconTheme: const IconThemeData(
             color: Colors.black,
-            fontWeight: FontWeight.w700,
           ),
         ),
-        iconTheme: const IconThemeData(
-          color : Colors.black,
-        ),
-      ),
-        body: FutureBuilder(
-          future: Api().payGet(),
-          builder: (BuildContext? context, AsyncSnapshot<Map<String, dynamic>?> snapshot) {
-            print(snapshot.data);
-            if(snapshot.hasData){
-              return ListView(
-                children: <Widget>[
-
-                ],
-              );
-            }
-            return Center(child: CupertinoActivityIndicator());
-          },
-        )
-    );
+        body: Container(
+          child: ListView(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Card(
+                  color: toastPay['color'],
+                  child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(toastPay["payName"], style: TextStyle(color: Colors.white, fontSize: 24,fontWeight: FontWeight.w500),),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          verticalDirection: VerticalDirection.up,
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text( "잔액  "+ toastPay["payCheck"], style: TextStyle(color: Colors.white, fontSize: 24,fontWeight: FontWeight.w500),),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 1,
+                color: Colors.white,
+                child: Center(
+                  child: ListView.builder(
+                    itemCount: payment.length,
+                    itemBuilder: (BuildContext ctx, int idx) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width * 0.95,
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        child: Card(
+                          color: payment[idx]['color'],
+                          child: Stack(
+                            children: [
+                              Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(payment[idx]["card"], style: TextStyle(color: Colors.white, fontSize: 24,fontWeight: FontWeight.w500),),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                              Column(
+                                verticalDirection: VerticalDirection.up,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(payment[idx]["cardNum"], style: TextStyle(color: Colors.white, fontSize: 24,fontWeight: FontWeight.w500),),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ]
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ lever_up_toast/lib/values/infoCard.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: file_names
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Map toastPay =    {
+  "payName": "ToastPay",
+  "payCheck": "100,000",
+  "color": Color(0xFF4C7241)
+};
+List payment = [
+  {"card": "VISA", "cardNum": "1234-1234-1234-1234", "color": Colors.blue},
+  {"card": "VISA", "cardNum": "1234-1234-1234-1234", "color": Colors.cyan},
+];
